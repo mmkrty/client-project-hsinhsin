88c7ef56aadf1e673f12973e5d3ef5b248132a96
Hao Chen <mmkrty@gmail.com>
2022-10-26 15:58:42 +0200

    start about and info component

---
+++ src/components/about/about.component.jsx
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <section className=" bg-secondary flex items-center justify-center">
+      <div className="min-h-screen max-w-7xl flex justify-center items-center">
+        <div>
+          <h2>Training</h2>
+          <p></p>
+          <h2>Experience</h2>
+          <ul>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div></div>
+      </div>
+    </section>
+  );
+};
+export default About;
